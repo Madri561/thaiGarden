8cab6aa74e71bcf460a040a77cf769ee95a1d1c1
Madri561 <145872630+Madri561@users.noreply.github.com>
2024-12-11 19:45:19 -0800

    init commit

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-// Serve static files from the 'public' directory
+
 app.use(express.static(path.join(__dirname, "public")));
 
-// Serve the index.html file
+
 app.get("/", (res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Endpoint to get the Google Maps URL
+
 app.get("/map", (req, res) => {
   const apiKey = "AIzaSyBA_YseNz-fwIDmOrBRm_sFE-lYGLqLAt8";
   const coordinates = "33.216490781025186,-117.2717647177939";
